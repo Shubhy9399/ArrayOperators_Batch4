a41aea801ae4958ee075b936895be2da8cdd9d47
Gireesh Chandrakar <55228190+gireesh9599@users.noreply.github.com>
2020-06-08 13:09:13 +0530

    Update ArraySorting.java

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,29 +1,17 @@
 
-public static void search(int[] arr,int searchElement) {
-	int first=0;
-    int last=arr.length-1;;
-    
-    
-    while (last>=first){  //loop untill all elements are traversed
-        int mid = (first + last )/2;  //calsulate the mid of elments
-        
-        if (arr[mid] == searchElement){  //compare mid element with search element
-      
-     
-    	System.out.println(searchElement +" is found at index: "+ mid);
+//bubble sort
 
-    	  break;
-        }  
-        if (searchElement > arr[mid]){  //compare mid element is largeer than search element or not 
-        	first = mid + 1;
-        }else{  
-        	last=mid-1;
-        }  
-    }  
-    
-    
-  
-        System.out.println(searchElement +" is not found");    
-    
-   
+void ArraySorting(int array[]){
+	int length=array.length();
+	int temp=0;
+
+	for(int i=0;i<length;i++){
+		for(int j=1;j<(length-1);j++)
+			if(array[j-1]>arr[j])
+			{
+				temp=array[j-1];
+				array[j-1]=array[j];
+				array[j]=temp;
+			}
+	}
 }
